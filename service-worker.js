8e753aa564c3700502d0064ce585d4c531153088
kristianduda <kristianduda83@gmail.com>
2020-12-21 15:41:29 +0100

    v0.3.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d7c645a2313d304dcae620221d27a28.js"
+  "/precache-manifest.622ffd566518c79d0cac2ec6843f0e79.js"
 );
 
 workbox.clientsClaim();
